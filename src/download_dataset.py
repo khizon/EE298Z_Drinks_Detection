f6fdc5bd307cf0479813ab5b4c92e19e95aa1460
kiel <kielhizon@gmail.com>
2022-04-23 08:22:47 +0000

    added download dataset

---
+++ src/download_dataset.py
@@ -0,0 +1,11 @@
+import os
+
+if __name__ == '__main__':
+    # Dataset
+    url = 'https://drive.google.com/drive/folders/1mN0v95iEKl0V7myAisuNjRaHIdEr_55O?usp=sharing'
+    data = 'data/'
+    
+    os.system(f'gdown --folder {url} -O {data}')
+    
+    os.system(f'tar -xzf data/drinks/drinks.tar.gz -C data/')
+    os.system(f'mv data/drinks data/imgs')
