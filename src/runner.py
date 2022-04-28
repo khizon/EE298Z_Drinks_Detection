20e9e761a97e39360800cd556c99620efbcd191e
khizon <kielhizon@gmail.com>
2022-04-28 09:44:33 +0000

    changed model used

--- src/runner.py
+++ src/runner.py
@@ -36,9 +36,9 @@ if __name__ == '__main__':
                 --test-only --resume artifacts/temp/checkpoint.pth  --data-augmentation none --project {project} > logs.txt'
     else:
         command = f'torchrun --nproc_per_node=1 src/train.py\
-            --dataset {dataset}--data-path data --model fasterrcnn_mobilenet_v3_large_320_fpn --epochs 26\
+            --dataset {dataset} --data-path data --model fasterrcnn_mobilenet_v3_large_320_fpn --epochs 26\
             --lr-steps 16 22 --aspect-ratio-group-factor 3\
-            --output-dir artifacts/temp --data-augmentation none --pretrained --project {project}> logs.txt'
+            --output-dir artifacts/temp --data-augmentation none --pretrained --project {project} > logs.txt'
     
     
     os.system('clear')
