a743cbc7878c28336afdefa10190c9cfaab3b770
khizon <kielhizon@gmail.com>
2022-04-29 11:43:27 +0000

    modified download_dataset.py

--- src/download_dataset.py
+++ src/download_dataset.py
@@ -5,7 +5,12 @@ if __name__ == '__main__':
     url = 'https://drive.google.com/drive/folders/1mN0v95iEKl0V7myAisuNjRaHIdEr_55O?usp=sharing'
     data = 'data/'
     
+    if not os.path.exists(data):
+        os.makedirs(data)
+    
+    os.system(f'clear')
     os.system(f'gdown --folder {url} -O {data}')
     
     os.system(f'tar -xzf data/drinks.tar.gz -C data/')
+    os.system(f'unzip -d data/ data/annotations.zip')
     os.system(f'mv data/drinks data/imgs')
