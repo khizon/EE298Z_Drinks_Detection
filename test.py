3646c01a18fc612d0e081a4c7dac8fb9f07b37e7
khizon <kielhizon@gmail.com>
2022-04-30 12:57:34 +0000

    git released models tested

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
     command = f'torchrun --nproc_per_node=1 src/train.py\
                 --dataset drinks --data-path data --model fasterrcnn_mobilenet_v3_large_fpn\
-                --test-only --resume https://github.com/khizon/EE298Z_Drinks_Detection/releases/download/v0.0/checkpoint.pth'
+                --test-only --resume https://github.com/khizon/EE298Z_Drinks_Detection/releases/download/v1.0/fasterrcnn_mobilenet_v3_large_fpn_none.pth'
     
     os.system('clear')
     os.system(command)
